3e2749ea1c541bf68ec266c03579bc2c11bd75ff
Megha Sahgal <meghas@gmail.com>
2022-10-14 03:33:14 +0000

    finished!

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,22 +1,39 @@
 import "./SingleArticle.css";
 import { useParams } from "react-router-dom";
-import ArticleList from "../ArticleList";
+import { useSelector } from "react-redux";
+import { findArticleById } from "../../store/articleReducer";
 
 // place articles as arg
-const SingleArticle = ({ articles }) => {
+const SingleArticle = () => {
   const { id } = useParams();
+  // clean way
+  const chosenArticle = useSelector(findArticleById(id));
+  // const chosenArticle = useSelector((store) => {
+  //   const articles = store.articleState.entries;
+  //   return articles.find((el) => el.id === id);
+  // });
+  // const articles = useSelector((store) => store.articleState.entries);
+  // find the article id equal to the param by passing props
+  //second way
+  // const chosenArticle = articles.find((el) => el.id === id);
+  // console.log(chosenArticle);
   return (
-    <div className="singleArticle">
-      <h1>{articles[id].title}</h1>
-      <img src={articles[id].imageUrl} alt="" />
-      <p>{articles[id].body}</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
-      </p>
-    </div>
+    <>
+      {chosenArticle && (
+        <div className="singleArticle">
+          <h1>{chosenArticle.title}</h1>
+          <img src={chosenArticle.imageUrl} alt="{chosenArticle.title}" />
+          <p>{chosenArticle.body}</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
+            laboriosam officia accusantium veritatis fugiat exercitationem vero
+            autem nihil aliquid ullam recusandae, quis odit odio voluptates
+            explicabo nobis! Consequuntur, aliquam?
+          </p>
+        </div>
+      )}
+      ;
+    </>
   );
 };
 
